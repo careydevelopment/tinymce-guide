c7cb65d6a83b788a3844ae0831dc24d46c9e295e
Carey Development, LLC <mrbrianmcarey@gmail.com>
2020-08-17 19:06:02 -0400

    Added bootstrap

---
+++ src/main/java/com/careydevelopment/pathvariableguide/PathVariableGuide.java
@@ -0,0 +1,12 @@
+package com.careydevelopment.pathvariableguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathVariableGuide {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathVariableGuide.class,args);
+	}
+}
