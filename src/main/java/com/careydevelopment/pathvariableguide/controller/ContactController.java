101bc25a051d33075f820b1a630cfac389c76a57
Carey Development, LLC <mrbrianmcarey@gmail.com>
2020-08-18 09:03:49 -0400

    Controller, model, service

---
+++ src/main/java/com/careydevelopment/pathvariableguide/controller/ContactController.java
@@ -0,0 +1,53 @@
+package com.careydevelopment.pathvariableguide.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.careydevelopment.pathvariableguide.model.Contact;
+import com.careydevelopment.pathvariableguide.service.ContactService;
+
+@RestController
+@RequestMapping("/contact")
+public class ContactController {
+
+	@Autowired
+	private ContactService contactService;
+	
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Contact> contact(@PathVariable Integer id) {
+		HttpStatus status = HttpStatus.OK;
+		Contact contact = contactService.fetchContactById(id);
+		
+		if (contact == null) {
+			status = HttpStatus.NOT_FOUND;
+		}
+
+		return ResponseEntity.status(status).body(contact);
+	}
+
+	
+	@GetMapping("/{salesOwnerId}/{id}")
+	public ResponseEntity<Contact> contactBySalesOwner(@PathVariable Integer salesOwnerId, @PathVariable Integer id) {
+		HttpStatus status = HttpStatus.OK;
+		Contact contact = null;
+		
+		Optional<Contact> contactOptional = contactService.fetchContactBySalesOwnerIdAndId(salesOwnerId, id);
+		
+		if (!contactOptional.isPresent()) {
+			status = HttpStatus.NOT_FOUND;
+		} else {
+			contact = contactOptional.get();
+		}
+
+		return ResponseEntity.status(status).body(contact);
+	}
+	
+}
---
+++ src/main/java/com/careydevelopment/pathvariableguide/service/ContactService.java
@@ -0,0 +1,60 @@
+package com.careydevelopment.pathvariableguide.service;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import org.springframework.stereotype.Service;
+
+import com.careydevelopment.pathvariableguide.model.Contact;
+
+@Service
+public class ContactService {
+
+	public Contact fetchContactById(Integer id) {
+		switch (id) {
+			case 1:
+				return fetchJimmyBuffet();
+			case 2:
+				return fetchDarthVader();
+			default:
+				return null;
+		}
+	}
+	
+	
+	public Optional<Contact> fetchContactBySalesOwnerIdAndId(Integer salesOwnerId, Integer id) {
+		Optional<Contact> contact = Stream.of(fetchJimmyBuffet(), fetchDarthVader())
+									.filter(c -> c.getSalesOwnerId().equals(salesOwnerId))
+									.filter(c -> c.getId().equals(id))
+									.findFirst();
+
+		return contact;
+	}
+	
+	
+	private Contact fetchJimmyBuffet() {
+		Contact contact = new Contact();
+		
+		contact.setId(1);
+		contact.setAuthority(true);
+		contact.setEmail("jimmy.buffet@xmail.com");
+		contact.setFirstName("James");
+		contact.setLastName("Buffet");
+		
+		return contact;
+	}
+
+	
+	private Contact fetchDarthVader() {
+		Contact contact = new Contact();
+		
+		contact.setId(2);
+		contact.setAuthority(true);
+		contact.setEmail("darth.vader@xmail.com");
+		contact.setFirstName("Darth");
+		contact.setLastName("Vader");
+		
+		return contact;
+	}
+
+}
